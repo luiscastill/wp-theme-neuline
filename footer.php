a79d96af55ca4e97db38384826c087005e1f34ba
luiscastill <eduardocaztillo0212@gmail.com>
2023-02-04 23:55:16 -0500

    initial commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+</main>
+    
+    
+    
+    <?php wp_footer();?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes()?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset')?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="This is the website of my chamber of commerce project.">
+    <?php wp_head();?>
+
+</head>
+
+<body>
+    <?php wp_body_open();?>
+   
+    <main>
