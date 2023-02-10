919a29308aba938c0bd3cd697c923a9d9e8b2023
luiscastill <eduardocaztillo0212@gmail.com>
2023-02-10 10:27:31 -0500

    initial commit

---
+++ template-parts/section-cta1.php
@@ -0,0 +1,12 @@
+<div class="cta1">
+    <div class="container cta1-container">
+        <div class="row">
+            <div class="col-12 col-sm-8 cta1-text">
+                <h2>Now Hiring Nurse Injectors</h2>
+            </div>
+            <div class="col-12 col-sm-4 cta1-btn">
+                <a href="http://">Apply Today</a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ template-parts/section-hero.php
@@ -0,0 +1,23 @@
+<div class="hero">
+    <div class="container hero-container">
+        <div class="row">
+            <div class="col-12 col-sm-6 hero-content">
+                <div class="hero-title">
+                    <h1>The beginning <span>and end</span> of your self-care search.</h1>
+                </div>
+                <div class="hero-buttons">
+                    <a href="http://">Call</a>
+                    <a href="http://">Email</a>
+                </div>
+                <div class="hero-subtitle">
+                    <p>Injectables</p>
+                    <p>Skincare</p>
+                    <p>Self-Care</p>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 hero-img">
+                <img src="<?php echo get_template_directory_uri().'/assets/img/hero-img.jpg';?>" alt="" srcset="">
+            </div>
+        </div>
+    </div>
+</div>
