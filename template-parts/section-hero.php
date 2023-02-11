314494d2076969a690565f5049133a07c742f6fb
luiscastill <eduardocaztillo0212@gmail.com>
2023-02-11 18:31:00 -0500

    initial commit

--- template-parts/section-hero.php
+++ template-parts/section-hero.php
@@ -1,7 +1,10 @@
 <div class="hero">
     <div class="container hero-container">
         <div class="row">
-            <div class="col-12 col-sm-6 hero-content">
+            <div class="col-12 col-lg-6 order-lg-12 hero-img">
+                <img src="<?php echo get_template_directory_uri().'/assets/img/Girl-Image-ColorCo.jpg';?>" alt="" srcset="">
+            </div>
+            <div class="col-12 col-lg-6 order-lg-1 hero-content">
                 <div class="hero-title">
                     <h1>The beginning <span>and end</span> of your self-care search.</h1>
                 </div>
@@ -14,9 +17,6 @@
                     <p>Skincare</p>
                     <p>Self-Care</p>
                 </div>
-            </div>
-            <div class="col-12 col-sm-6 hero-img">
-                <img src="<?php echo get_template_directory_uri().'/assets/img/hero-img.jpg';?>" alt="" srcset="">
             </div>
         </div>
     </div>
